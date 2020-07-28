f8aa1f584470e73d456338d2f47ef435a659edc5
jasper-zanjani <jasper.zanjani@gmail.com>
2020-07-28 12:46:42 -0400

    Refactoring

--- Program.cs
+++ Program.cs
@@ -3,13 +3,19 @@
 namespace csharp {
   class Program {
     static void Main(string[] args) {
+      Console.WriteLine("Hello World");
+      getname();
+    }
+    static void getname() {
       Console.WriteLine("What's your name?");
       string name = Console.ReadLine();
       Console.WriteLine("Hello, {0}!",name);
+    }
+    static void getage() {
       Console.WriteLine("How old are you?");
       int age = int.Parse(Console.ReadLine());
       age += 1;
-      Console.WriteLine("Next year you'll be {0} years old",age);
+      Console.WriteLine("Next year you'll be {0} years old",age.ToString());
       age += 1;
-      Console.WriteLine("and the year after you'll be {0}}!",age);
+      Console.WriteLine("and the year after you'll be {0}}!",age.ToString());
 } } }
