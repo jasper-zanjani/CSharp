af92445b1e9440bd58b9d62f5b633bea05008287
jasper-zanjani <jasper.zanjani@gmail.com>
2020-07-28 11:43:05 -0400

    Converting string to int

--- Program.cs
+++ Program.cs
@@ -6,4 +6,10 @@ namespace csharp {
       Console.WriteLine("What's your name?");
       string name = Console.ReadLine();
       Console.WriteLine("Hello, {0}!",name);
+      Console.WriteLine("How old are you?");
+      int age = int.Parse(Console.ReadLine());
+      age += 1;
+      Console.WriteLine("Next year you'll be {0} years old",age);
+      age += 1;
+      Console.WriteLine("and the year after you'll be {0}}!",age);
 } } }
