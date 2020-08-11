a88756c797b72037bad9d88c953488479e23a147
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-08-11 19:13:40 -0400

    Navigating between multiple pages using Frame.Navigate

--- UWP/learning-uwp/Page2.xaml.cs
+++ UWP/learning-uwp/Page2.xaml.cs
@@ -13,23 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace learning_uwp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Page2 : Page
     {
-        public MainPage()
+        public Page2()
         {
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            Frame.Navigate(typeof(Page3));
         }
     }
 }
