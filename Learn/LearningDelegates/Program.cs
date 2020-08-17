853b8204a3ecbeb48ce0e5c51f25716daddfe3ce
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-08-17 18:47:24 -0400

    :brain::desktop_computer: End of module 3

--- Learn/LearningDelegates/Program.cs
+++ Learn/LearningDelegates/Program.cs
@@ -11,11 +11,18 @@ namespace DelegatesAndEvents
     static void Main(string[] args)
     {
       var worker = new Worker();
-      worker.DoWork(2, WorkType.Golf);
+      worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+      {
+        Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+      };
+      worker.WorkCompleted += delegate (object sender, EventArgs e)
+      {
+        Console.WriteLine("Worker is done");
+      };
+      worker.DoWork(8, WorkType.GenerateReports);
 
       Console.Read();
     }
-
   }
 
   public enum WorkType
--- Learn/LearningDelegates/Worker.cs
+++ Learn/LearningDelegates/Worker.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public class Worker
+  public class Worker
+  {
+    public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+    public event EventHandler WorkCompleted;
+
+    public void DoWork(int hours, WorkType workType)
     {
-        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
-        public event EventHandler WorkCompleted;
-
-        public void DoWork(int hours, WorkType workType)
-        {
-            for (int i = 0; i < hours; i++)
-            {
-                System.Threading.Thread.Sleep(1000);
-                OnWorkPerformed(i + 1, workType);
-            }
-            OnWorkCompleted();
-        }
-
-        protected virtual void OnWorkPerformed(int hours, WorkType workType)
-        {
-            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
-            if (del != null)
-            {
-                del(this, new WorkPerformedEventArgs(hours, workType));
-            }
-        }
-
-        protected virtual void OnWorkCompleted()
-        {
-            var del = WorkCompleted as EventHandler;
-            if (del != null)
-            {
-                del(this, EventArgs.Empty);
-            }
-        }
+      for (int i = 0; i < hours; i++)
+      {
+        System.Threading.Thread.Sleep(1000);
+        OnWorkPerformed(i + 1, workType);
+      }
+      OnWorkCompleted();
     }
+
+    protected virtual void OnWorkPerformed(int hours, WorkType workType)
+    {
+      var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+      if (del != null)
+      {
+        del(this, new WorkPerformedEventArgs(hours, workType));
+      }
+    }
+
+    protected virtual void OnWorkCompleted()
+    {
+      var del = WorkCompleted as EventHandler;
+      if (del != null)
+      {
+        del(this, EventArgs.Empty);
+      }
+    }
+  }
 }
