b49ef0df74725cb756612123ce50b318f7cda7ff
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-08-17 15:45:01 -0400

    :brain::stars: Starting new project for learning delegates and events

---
+++ Learn/LearningDelegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LearningDelegates
+{
+  public enum WorkType { Golf, Meeting, Report }
+  public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+  class Program
+  {
+    static void WorkPerformed1( int hours, WorkType workType)
+    {
+      Console.WriteLine("WorkPerformed1 called");
+    }
+
+    static void WorkPerformed2(int hours, WorkType workType)
+    {
+      Console.WriteLine("WorkPerformed2 called");
+    }
+
+    static void Main(string[] args)
+    {
+      WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+      WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+      del1(1, WorkType.Golf);
+      del2(2, WorkType.Report);
+    }
+  }
+}
