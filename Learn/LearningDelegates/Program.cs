a5f63c713c86a19208d4d6bdf1bbae1f2f36f328
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-08-17 19:28:59 -0400

    :brain::rocket: Trying to wire a new handler to event

--- Learn/LearningDelegates/Program.cs
+++ Learn/LearningDelegates/Program.cs
@@ -11,11 +11,17 @@ namespace DelegatesAndEvents
     static void Main(string[] args)
     {
       var enterpriseMission = new StarshipMission();
+      enterpriseMission.MissionStatusReport += new EventHandler<MissionStatusReportEventArgs>(FirstContact);
+
       enterpriseMission.StartMission(2, MissionType.Exploration);
 
       Console.Read();
     }
 
+    static void FirstContact(int distance, MissionType missiontype)
+    {
+      Console.WriteLine("Making first contact!");
+    }
   }
 
   public enum MissionType
